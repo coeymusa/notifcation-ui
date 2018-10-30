db6f87a1d4d31c6aeef8116f2d099e6546742c77
coeymusa <coreymusa@outlook.com>
2018-10-30 17:10:55 +0000

    post light state to backend

--- src/Light.js
+++ src/Light.js
@@ -4,10 +4,29 @@ export default class Light extends React.Component {
     constructor(props){
         super(props)
     }
-    
+
+    handleClick(){
+
+    fetch('http://localhost:7077/lights', {
+        mode: 'cors',
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.props.light)
+    }).then(res=>res.json())
+        .then(res => console.log(res));
+}
+
+
     render() {
         return (
-            <div>
+              <div>
+
+                    <button onClick={(e) => this.handleClick(e)}>
+                        Turn On/Off
+                    </button>
                 <td>Name: {this.props.light.name} </td>
                 <td>Colour: {this.props.light.xy[0]}{this.props.light.xy[1]}  </td>
                 <td>On: {this.props.light.on}</td>
@@ -16,3 +35,4 @@ export default class Light extends React.Component {
         )
     }
 }
+//      <img src="./image.jpg"/>
